6caa27ca403fbe761ea73242ded48f41a763c226
Shivam Goel <55030452+shivamgoel008@users.noreply.github.com>
2021-05-09 16:41:12 +0530

    signin and login complete

--- app/src/main/java/com/example/venom/login/LoginActivity.java
+++ app/src/main/java/com/example/venom/login/LoginActivity.java
@@ -2,7 +2,9 @@ package com.example.venom.login;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,11 +17,13 @@ import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity<cardviewSignup> extends AppCompatActivity {
 
 
-    private TextInputEditText userPassword;
+    private TextInputEditText userPassword,userEmail;
+    private CardView cardviewSignup;
     private String email,password;
+
 
     private Button loginButton;
 
@@ -28,45 +32,44 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        TextInputEditText userEmail=(TextInputEditText)findViewById(R.id.person_email);
-        TextInputEditText userPassword =(TextInputEditText)findViewById(R.id.person_password);
-
+        userEmail=(TextInputEditText)findViewById(R.id.person_email);
+        userPassword =(TextInputEditText)findViewById(R.id.person_password);
+        cardviewSignup=(CardView)findViewById(R.id.cardView_signup);
         loginButton=(Button)findViewById(R.id.login_button);
 
-        loginButton.setOnClickListener(new View.OnClickListener() {
+        loginButton.setOnClickListener(v -> {
 
-            @Override
-            public void onClick(View v) {
+            Toast.makeText(LoginActivity.this, "button clicked", Toast.LENGTH_SHORT).show();
+            email=userEmail.getText().toString().trim();
+            password=userPassword.getText().toString().trim();
 
-                Toast.makeText(LoginActivity.this, "button clicked", Toast.LENGTH_SHORT).show();
-                email=userEmail.getText().toString().trim();
-                password=userPassword.getText().toString().trim();
+            if(email.equals("")){
+                userEmail.setError("Enter Email");
+            }
+            else if(userPassword.equals("")){
+                userPassword.setError("Enter correct Password");
+            }
 
-                if(email.equals("")){
-                    userEmail.setError("Enter Email");
-                }
-                else if(userPassword.equals("")){
-                    userPassword.setError("Enter correct Password");
-                }
+            else{
+                FirebaseAuth firebaseAuth=FirebaseAuth.getInstance();
+                firebaseAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
 
-                else{
-                    FirebaseAuth firebaseAuth=FirebaseAuth.getInstance();
-                    firebaseAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if(task.isSuccessful()){
-
-                            }
-
-                            else{
-                                Toast.makeText(LoginActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
-                            }
                         }
-                    });
-                }
+
+                        else{
+                            Toast.makeText(LoginActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
             }
         });
 
+        cardviewSignup.setOnClickListener(v->{
+            startActivity(new Intent(this,SignupActivity.class));
+        });
     }
 
 }
--- app/src/main/java/com/example/venom/login/SignupActivity.java
+++ app/src/main/java/com/example/venom/login/SignupActivity.java
@@ -1,16 +1,83 @@
 package com.example.venom.login;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.venom.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.regex.Pattern;
 
 public class SignupActivity extends AppCompatActivity {
+
+    private TextInputEditText signupName, signupEmail, signupPassword, signupConfirmPassword;
+    private Button signupButton;
+    private String name, email, password, confirmPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        signupName = (TextInputEditText) findViewById(R.id.signup_name);
+        signupEmail = (TextInputEditText) findViewById(R.id.signup_email);
+        signupPassword = (TextInputEditText) findViewById(R.id.signup_password);
+        signupConfirmPassword = (TextInputEditText) findViewById(R.id.signup_confirm_password);
+        signupButton = (Button) findViewById(R.id.signup_button);
+
+
+        signupButton.setOnClickListener(v -> {
+            name = signupName.getText().toString().trim();
+            email = signupEmail.getText().toString().trim();
+            password = signupPassword.getText().toString().trim();
+            confirmPassword = signupConfirmPassword.getText().toString().trim();
+
+            if (name.equals("")) {
+                signupName.setError("Enter Valid Name");
+            }
+
+            else if (email.equals("")) {
+                signupEmail.setError("Enter Valid Email");
+            }
+
+            else if (password.equals("")) {
+                signupPassword.setError("Enter String Password");
+            }
+
+            else if (!(confirmPassword.equals(password))) {
+                signupConfirmPassword.setError("Password Don't Match");
+            }
+
+            else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                signupEmail.setError("Enter Correct Email");
+            }
+
+            else {
+                FirebaseAuth firebaseAuth=FirebaseAuth.getInstance();
+                firebaseAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(SignupActivity.this, "User Created Successfully", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(SignupActivity.this,LoginActivity.class));
+                        }
+                        else {
+                            Toast.makeText(SignupActivity.this, "Signup Failed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
     }
 }
